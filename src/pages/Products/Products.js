72d780ee1afc5ca21fca0104ac4364eecadadc96
Hatice AKGÜL <katrancihatice@icloud.com>
2025-03-05 04:19:03 +0300

    Add component

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,32 +1,39 @@
-import react , {useState, useEffect} from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import react from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, ActivityIndicator } from 'react-native';
 import { API_URL } from '@env';
 import Config from 'react-native-config'
-import axios from 'axios';
 import ProductCard from '../../components/ProductCard/ProductCard';
+import useFetch from '../../hook/useFetch/useFetch';
+import Loading from '../../components/Loading/Loading';
+import Error from '../../components/Error/Error';
 
 
-
-const Products = () => {   
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        fetchData();
-    }, []); 
-    
-    const fetchData = async () => {
-        const {data: productData}= await axios.get(API_URL);
-        setData(productData);
-
-    };
+const Products = () => {
+    const {loading, data, error} = useFetch(API_URL);
+    // console.log("render");
+    // console.log({loading, data: data.length, error});
+    // console.log('--------------------')
 
 
-    const renderProduct = ({item}) => <ProductCard product={item} />;
+    const renderProduct = ({ item }) => <ProductCard product={item} />
+
+    if (loading) {
+        return (
+            <Loading></Loading>
+        );
+    }
+    if(error){
+        return (
+            <Error></Error>
+        );
+    }
+
+
 
 
     return (
         <SafeAreaView >
-            <FlatList data={data} renderItem={renderProduct}/>
+            <FlatList data={data} renderItem={renderProduct} />
         </SafeAreaView>
     );
 }
