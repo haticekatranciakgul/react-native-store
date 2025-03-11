36bad1cf6239da6e45bf6e5cd975323850faf176
Hatice AKGÜL <katrancihatice@icloud.com>
2025-03-11 22:34:50 +0300

    Update login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { SafeAreaView, Text, View, Image } from 'react-native';
+import { Formik } from 'formik';
 import styles from './Login.style';
 import Input from '../../components/Input/Input'
 import Button from '../../components/Button/Button';
 
 const Login = () => {
-    console.log('here')
+    function handleLogin(values){
+        console.log(values)
+    }
 
     return (
         <SafeAreaView style={styles.container} >
             <View style={styles.logo_container}>
                 <Image style={styles.logo} source={require('../../assets/login-logo.png')} />
             </View>
-            <View style={styles.body_container}>
-                <Input placeholder="Kullanıcı adı giriniz" />
-                <Input placeholder="Şifre giriniz" />
-                <Button text="Giriş Yap"></Button>
-            </View>
+            <Formik initialValues={{ username: '', password: '' }}
+                onSubmit={handleLogin}>
+                {({ handleSubmit, handleChange, values }) => (
+                    <View style={styles.body_container}>
+                        <Input placeholder="Kullanıcı adı giriniz"
+                            value={values.username}
+                            onType={handleChange('username')} />
+                        <Input placeholder="Şifre giriniz"
+                            value={values.password}
+                            onType={handleChange('password')} />
+                        <Button text="Giriş Yap" onPress={handleSubmit} ></Button>
+                    </View>
+                )}
+            </Formik>
         </SafeAreaView>
 
     )
